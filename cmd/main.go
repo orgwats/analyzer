95d9cfbb02456a4bee87b3fc1a20ef1e78d8c736
waik1ki <arbutus@outlook.kr>
2025-03-24 18:43:26 +0900

    feat: 서버 실행 로직 추가

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"log"
+	"net"
+	"sync"
+
+	"google.golang.org/grpc"
+
+	"github.com/orgwats/analyzer/internal/config"
+	"github.com/orgwats/analyzer/internal/gapi"
+	aPb "github.com/orgwats/idl/gen/go/analyzer"
+)
+
+func main() {
+	// TODO 임시
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	// TODO 임시
+	store := struct{}{}
+	server := gapi.NewServer(cfg, store)
+	grpcServer := grpc.NewServer()
+
+	aPb.RegisterAnalyzerServer(grpcServer, server)
+
+	listener, err := net.Listen("tcp", ":50052")
+	if err != nil {
+		log.Fatal("cannot listen network address:", err)
+	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		log.Printf("start analyzer GRPC server at %s", listener.Addr().String())
+
+		if err := grpcServer.Serve(listener); err != nil {
+			log.Fatal("market server failed to serve:", err)
+		}
+	}()
+	wg.Wait()
+}
