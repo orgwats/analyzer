8426c4b1c4bd873507b7cea5a6356f9e3f5a1d3b
waik1ki <arbutus@outlook.kr>
2025-03-28 22:05:11 +0900

    feat: 구조체 업데이트 및 kafka 주문 로직 추가

--- internal/analyzer/analyzer.go
+++ internal/analyzer/analyzer.go
@@ -3,24 +3,38 @@ package analyzer
 import (
 	"context"
 	"io"
-	"log"
+	"strconv"
 
 	"github.com/google/uuid"
+	"github.com/orgwats/analyzer/internal/kafka"
 	mPb "github.com/orgwats/idl/gen/go/market"
 	"google.golang.org/grpc"
 )
 
 type Analyzer struct {
+	Id    string
 	ctx   context.Context
 	store interface{}
 
-	client mPb.MarketClient
-	stream grpc.ServerStreamingClient[mPb.Candle]
+	symbol string
 
-	symbol       string
+	client   mPb.MarketClient
+	stream   grpc.ServerStreamingClient[mPb.Candle]
+	producer *kafka.Producer
+
+	Position *Position
+
 	CandleBuffer *CandleBuffer
 
 	Stop func() error
+}
+
+type Position struct {
+	Type       string // "LONG", "SHORT", ""
+	Price      float64
+	TakeProfit float64
+	StopLoss   float64
+	IsFilled   bool
 }
 
 func NewAnalyzer(
@@ -28,16 +42,25 @@ func NewAnalyzer(
 	store interface{},
 	client mPb.MarketClient,
 	stream grpc.ServerStreamingClient[mPb.Candle],
+	producer *kafka.Producer,
 	symbol string,
 	stop func() error,
-) (uuid.UUID, *Analyzer) {
-	uuid, _ := uuid.NewUUID()
+) (string, *Analyzer) {
 	analyzer := &Analyzer{
-		ctx:    ctx,
-		store:  store,
-		client: client,
-		stream: stream,
-		symbol: symbol,
+		Id:       uuid.New().String(),
+		ctx:      ctx,
+		store:    store,
+		client:   client,
+		stream:   stream,
+		producer: producer,
+		symbol:   symbol,
+		Position: &Position{
+			Type:       "",
+			Price:      0,
+			TakeProfit: 0,
+			StopLoss:   0,
+			IsFilled:   false,
+		},
 		CandleBuffer: &CandleBuffer{
 			buf:  make([]*mPb.Candle, 200),
 			head: 0,
@@ -46,7 +69,7 @@ func NewAnalyzer(
 		Stop: stop,
 	}
 
-	return uuid, analyzer
+	return analyzer.Id, analyzer
 }
 
 func (a *Analyzer) Init() error {
@@ -78,17 +101,103 @@ func (a *Analyzer) Run() {
 				return
 			}
 			if err != nil {
-				// 필요에 따라 재시도 로직
 				return
 			}
 
 			a.CandleBuffer.UpdateLastCandle(candle)
 
-			log.Printf("[%s] RSI : %f", a.symbol, CalculateRSI(a.CandleBuffer.GetCandles(), 14))
+			if a.Position.Type == "" {
+				orderSide := ""
+
+				rsi := CalculateRSI(a.CandleBuffer.GetCandles(), 14)
+
+				if rsi < 25 {
+					orderSide = "BUY"
+				}
+
+				if orderSide != "" {
+					price, _ := strconv.ParseFloat(candle.Close, 64)
+
+					if orderSide == "BUY" {
+						// config 에서 받아와서 초기화 해야함.
+						takeProfitROI := 1.0005
+						stopLossROI := 0.9998
+
+						a.Position.Type = "LONG"
+						a.Position.Price = price
+						a.Position.TakeProfit = price * takeProfitROI
+						a.Position.StopLoss = price * stopLossROI
+						a.Position.IsFilled = false
+					}
+
+					if orderSide == "SELL" {
+						// config 에서 받아와서 초기화 해야함.
+						takeProfitROI := 0.9998
+						stopLossROI := 1.0005
+
+						a.Position.Type = "SHORT"
+						a.Position.Price = price
+						a.Position.TakeProfit = price * takeProfitROI
+						a.Position.StopLoss = price * stopLossROI
+						a.Position.IsFilled = false
+					}
+
+					err = a.sendOrder(orderSide)
+					if err != nil {
+						// 필요에 따라 재시도 로직
+					}
+				}
+			} else {
+				price, _ := strconv.ParseFloat(candle.Close, 64)
+
+				if a.Position.Type == "LONG" {
+					if price >= a.Position.TakeProfit || price < a.Position.StopLoss {
+						err := a.sendOrder("SELL")
+						if err != nil {
+							// 필요에 따라 재주문 로직
+						}
+						a.resetPosition()
+					}
+				}
+
+				if a.Position.Type == "SHORT" {
+					if price <= a.Position.TakeProfit || price > a.Position.StopLoss {
+						err := a.sendOrder("BUY")
+						if err != nil {
+							// 필요에 따라 재주문 로직
+						}
+						a.resetPosition()
+					}
+				}
+			}
 
 			if candle.Closed {
 				a.CandleBuffer.AddCandle(candle)
 			}
 		}
 	}
+}
+
+func (a *Analyzer) resetPosition() {
+	a.Position.Type = ""
+	a.Position.Price = 0
+	a.Position.TakeProfit = 0
+	a.Position.StopLoss = 0
+	a.Position.IsFilled = false
+}
+
+func (a *Analyzer) sendOrder(side string) error {
+	order := &kafka.Order{
+		OrderSide: side,
+		Key:       uuid.New().String(),
+		Symbol:    a.symbol,
+		Size:      "0.01", // 테스트 고정 사이즈
+	}
+
+	_, _, err := a.producer.Send(a.Id, order)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
